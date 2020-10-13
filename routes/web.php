e2ccb1b82cb9d8696464b0afa6f6ea468365026b
mg-abramishvili <mg@abramishvili.net>
2020-10-13 19:31:25 +0500

    2020-10-13

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/home');
 });
 
-Auth::routes();
+Auth::routes([
+    'register' => false,
+    'reset' => false
+]);
 
 Route::view('/home', 'home')->middleware('auth');
